de589a834f1c2c04ce0525a342f59c97960c45f3
WyvernAllow <bloodflame2535@gmail.com>
2024-08-05 17:30:41 +0200

    window

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,33 @@
 #include <spdlog/spdlog.h>
 
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+static void on_glfw_error(int error, const char* description) {
+	spdlog::error("GLFW Error: {}", description);
+}
+
 int main(int argc, char** argv) {
-	spdlog::info("Hello, World!\n");
+	glfwSetErrorCallback(on_glfw_error);
+
+	if (!glfwInit()) {
+		spdlog::critical("Failed to initialize GLFW.");
+		return 1;
+	}
+
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+	GLFWwindow* window = glfwCreateWindow(800, 450, "Voxel Raytracer", nullptr, nullptr);
+	if (!window) {
+		spdlog::critical("Failed to create window.");
+		return 1;
+	}
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
 	return 0;
 }
