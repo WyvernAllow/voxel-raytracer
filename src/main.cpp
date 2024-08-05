52c4a7cf0247591156ece3ca5c53e0d1647a66c8
WyvernAllow <bloodflame2535@gmail.com>
2024-08-05 20:06:43 +0200

    instance creation

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,121 @@
 
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
+#include <vulkan/vulkan.h>
+#include <vulkan/vk_enum_string_helper.h>
+
+static VKAPI_ATTR VkBool32 VKAPI_CALL on_vk_debug_callback(
+	VkDebugUtilsMessageSeverityFlagBitsEXT severity,
+	VkDebugUtilsMessageTypeFlagsEXT type,
+	const VkDebugUtilsMessengerCallbackDataEXT* data,
+	void* user_data) {
+
+	switch (severity)
+	{
+	case VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT:
+		spdlog::trace("Vulkan: {}", data->pMessage);
+		break;
+	case VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT:
+		spdlog::info("Vulkan: {}", data->pMessage);
+		break;
+	case VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT:
+		spdlog::warn("Vulkan: {}", data->pMessage);
+		break;
+	case VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT:
+		spdlog::error("Vulkan: {}", data->pMessage);
+		break;
+	}
+
+	return VK_FALSE;
+}
+
+static VkResult CreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pDebugMessenger) {
+	auto func = (PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
+	if (func != nullptr) {
+		return func(instance, pCreateInfo, pAllocator, pDebugMessenger);
+	}
+	else {
+		return VK_ERROR_EXTENSION_NOT_PRESENT;
+	}
+}
+
+static void DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT debugMessenger, const VkAllocationCallbacks* pAllocator) {
+	auto func = (PFN_vkDestroyDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkDestroyDebugUtilsMessengerEXT");
+	if (func != nullptr) {
+		func(instance, debugMessenger, pAllocator);
+	}
+}
+
+static VkDebugUtilsMessengerCreateInfoEXT get_debug_utils_messenger_create_info() {
+	VkDebugUtilsMessengerCreateInfoEXT create_info{};
+	create_info.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+
+	create_info.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT 
+		| VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT 
+		| VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+
+	create_info.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT 
+		| VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT 
+		| VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+
+	create_info.pfnUserCallback = on_vk_debug_callback;
+	create_info.pUserData = nullptr;
+
+	return create_info;
+}
 
 static void on_glfw_error(int error, const char* description) {
 	spdlog::error("GLFW Error: {}", description);
+}
+
+static VkInstance create_instance(VkDebugUtilsMessengerCreateInfoEXT& debug_utils_messenger_create_info) {
+	VkApplicationInfo app_info{};
+	app_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+	app_info.apiVersion = VK_API_VERSION_1_0;
+
+	VkInstanceCreateInfo instance_info{};
+	instance_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+	instance_info.pApplicationInfo = &app_info;
+
+	uint32_t glfw_extension_count = 0;
+	const char** glfw_extensions;
+
+	glfw_extensions = glfwGetRequiredInstanceExtensions(&glfw_extension_count);
+
+	std::vector<const char*> extensions(glfw_extensions, glfw_extensions + glfw_extension_count);
+	extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+
+	instance_info.enabledExtensionCount = extensions.size();
+	instance_info.ppEnabledExtensionNames = extensions.data();
+
+	const std::vector<const char*> validation_layers = {
+		"VK_LAYER_KHRONOS_validation"
+	};
+
+	instance_info.enabledLayerCount = validation_layers.size();
+	instance_info.ppEnabledLayerNames = validation_layers.data();
+
+	instance_info.pNext = &debug_utils_messenger_create_info;
+
+	VkInstance instance;
+	VkResult result = vkCreateInstance(&instance_info, nullptr, &instance);
+	if (result != VK_SUCCESS) {
+		spdlog::error("Failed to create VkInstance. {}", string_VkResult(result));
+		return nullptr;
+	}
+
+	return instance;
+}
+
+static VkDebugUtilsMessengerEXT create_debug_utils_messenger(VkInstance instance, VkDebugUtilsMessengerCreateInfoEXT& debug_utils_messenger_create_info) {
+	VkDebugUtilsMessengerEXT messenger;
+	VkResult result = CreateDebugUtilsMessengerEXT(instance, &debug_utils_messenger_create_info, nullptr, &messenger);
+	if (result != VK_SUCCESS) {
+		spdlog::error("Failed to create VkDebugUtilsMessengerEXT. {}", string_VkResult(result));
+		return nullptr;
+	}
+
+	return messenger;
 }
 
 int main(int argc, char** argv) {
@@ -22,10 +134,27 @@ int main(int argc, char** argv) {
 		spdlog::critical("Failed to create window.");
 		return 1;
 	}
+	
+	VkDebugUtilsMessengerCreateInfoEXT debug_utils_info = get_debug_utils_messenger_create_info();
+
+	VkInstance instance = create_instance(debug_utils_info);
+	if (!instance) {
+		spdlog::critical("Cannot proceed without a valid instance.");
+		return 1;
+	}
+
+	VkDebugUtilsMessengerEXT debug_messenger = create_debug_utils_messenger(instance, debug_utils_info);
+	if (!debug_messenger) {
+		spdlog::critical("Cannot proceed without a debug utils messenger.");
+		return 1;
+	}
 
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
 	}
+
+	DestroyDebugUtilsMessengerEXT(instance, debug_messenger, nullptr);
+	vkDestroyInstance(instance, nullptr);
 
 	glfwDestroyWindow(window);
 	glfwTerminate();
