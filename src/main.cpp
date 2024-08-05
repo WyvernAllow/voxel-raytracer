923182ada9e71d207923e0be85c7fdfa6865acdf
WyvernAllow <bloodflame2535@gmail.com>
2024-08-05 20:30:39 +0200

    physical device selection and device creation

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,16 @@
 #include <vulkan/vulkan.h>
 #include <vulkan/vk_enum_string_helper.h>
 
+#include <vector>
+#include <optional>
+#include <set>
+#include <algorithm>
+#include <limits>
+
+static const std::vector<const char*> validation_layers = {
+	"VK_LAYER_KHRONOS_validation"
+};
+
 static VKAPI_ATTR VkBool32 VKAPI_CALL on_vk_debug_callback(
 	VkDebugUtilsMessageSeverityFlagBitsEXT severity,
 	VkDebugUtilsMessageTypeFlagsEXT type,
@@ -89,10 +99,6 @@ static VkInstance create_instance(VkDebugUtilsMessengerCreateInfoEXT& debug_util
 	instance_info.enabledExtensionCount = extensions.size();
 	instance_info.ppEnabledExtensionNames = extensions.data();
 
-	const std::vector<const char*> validation_layers = {
-		"VK_LAYER_KHRONOS_validation"
-	};
-
 	instance_info.enabledLayerCount = validation_layers.size();
 	instance_info.ppEnabledLayerNames = validation_layers.data();
 
@@ -117,6 +123,102 @@ static VkDebugUtilsMessengerEXT create_debug_utils_messenger(VkInstance instance
 	}
 
 	return messenger;
+}
+
+struct queue_family_indices {
+	std::optional<uint32_t> graphics_family;
+
+	bool is_complete() const {
+		return graphics_family.has_value();
+	}
+};
+
+static queue_family_indices find_queue_families(VkPhysicalDevice physdev) {
+	queue_family_indices indices;
+
+	uint32_t queue_family_count = 0;
+	vkGetPhysicalDeviceQueueFamilyProperties(physdev, &queue_family_count, nullptr);
+
+	std::vector<VkQueueFamilyProperties> queue_families(queue_family_count);
+	vkGetPhysicalDeviceQueueFamilyProperties(physdev, &queue_family_count, queue_families.data());
+
+	for (size_t i = 0; i < queue_family_count; i++) {
+		if (queue_families[i].queueFlags & VK_QUEUE_GRAPHICS_BIT && !indices.graphics_family.has_value()) {
+			indices.graphics_family = i;
+		}
+	}
+
+	return indices;
+}
+
+static int rate_physical_device(VkPhysicalDevice physdev) {
+	VkPhysicalDeviceProperties properties;
+	vkGetPhysicalDeviceProperties(physdev, &properties);
+
+	queue_family_indices indices = find_queue_families(physdev);
+	if (!indices.is_complete()) {
+		return -1;
+	}
+
+	int score = 0;
+
+	if (properties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU) {
+		score += 100;
+	}
+
+	return score;
+}
+
+static VkPhysicalDevice find_physical_device(VkInstance instance) {
+	uint32_t physical_device_count = 0;
+	vkEnumeratePhysicalDevices(instance, &physical_device_count, nullptr);
+
+	std::vector<VkPhysicalDevice> physical_devices(physical_device_count);
+	vkEnumeratePhysicalDevices(instance, &physical_device_count, physical_devices.data());
+
+	VkPhysicalDevice best_device = nullptr;
+	int highest_score = std::numeric_limits<int>::min();
+	for (VkPhysicalDevice physdev : physical_devices) {
+		int score = rate_physical_device(physdev);
+		if (score > highest_score) {
+			highest_score = score;
+			best_device = physdev;
+		}
+	}
+
+	return best_device;
+}
+
+static VkDevice create_device(VkPhysicalDevice physdev, queue_family_indices indices) {
+	VkDeviceQueueCreateInfo queue_create_info{};
+	queue_create_info.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+	queue_create_info.queueFamilyIndex = indices.graphics_family.value();
+	queue_create_info.queueCount = 1;
+
+	float queue_priority = 1.0f;
+	queue_create_info.pQueuePriorities = &queue_priority;
+
+	VkPhysicalDeviceFeatures device_features{};
+	
+	VkDeviceCreateInfo create_info{};
+	create_info.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+	create_info.pQueueCreateInfos = &queue_create_info;
+	create_info.queueCreateInfoCount = 1;
+	create_info.pEnabledFeatures = &device_features;
+
+	create_info.enabledExtensionCount = 0;
+
+	create_info.enabledLayerCount = validation_layers.size();
+	create_info.ppEnabledLayerNames = validation_layers.data();
+
+	VkDevice device;
+	VkResult result = vkCreateDevice(physdev, &create_info, nullptr, &device);
+	if (result != VK_SUCCESS) {
+		spdlog::error("Failed to create device. {}", string_VkResult(result));
+		return nullptr;
+	}
+
+	return device;
 }
 
 int main(int argc, char** argv) {
@@ -149,10 +251,35 @@ int main(int argc, char** argv) {
 		return 1;
 	}
 
+	VkPhysicalDevice physdev = find_physical_device(instance);
+	if (!physdev) {
+		spdlog::critical("Failed to find a suitable physical device.");
+		return 1;
+	}
+
+	VkPhysicalDeviceProperties physdev_props;
+	vkGetPhysicalDeviceProperties(physdev, &physdev_props);
+
+	spdlog::info("Selected physical device name: {}", physdev_props.deviceName);
+	spdlog::info("Selected physical device type: {}", string_VkPhysicalDeviceType(physdev_props.deviceType));
+
+	queue_family_indices physdev_indices = find_queue_families(physdev);
+	spdlog::info("Graphics family index: {}", physdev_indices.graphics_family.value());
+
+	VkDevice device = create_device(physdev, physdev_indices);
+	if (!device) {
+		spdlog::critical("Cannot proceed without a device.");
+		return 1;
+	}
+
+	VkQueue graphics_queue;
+	vkGetDeviceQueue(device, physdev_indices.graphics_family.value(), 0, &graphics_queue);
+
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
 	}
 
+	vkDestroyDevice(device, nullptr);
 	DestroyDebugUtilsMessengerEXT(instance, debug_messenger, nullptr);
 	vkDestroyInstance(instance, nullptr);
 
