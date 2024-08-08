0b30870593255f58537da3fc51cac4d984b9cad8
WyvernAllow <bloodflame2535@gmail.com>
2024-08-08 16:15:36 +0200

    define extension function

---
+++ src/extensions.cpp
@@ -0,0 +1,20 @@
+#include <vulkan/vulkan.h>
+
+VkResult vkCreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pDebugMessenger) {
+    auto func = (PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
+    if (!func) {
+        return VK_ERROR_EXTENSION_NOT_PRESENT;
+
+    }
+
+    return func(instance, pCreateInfo, pAllocator, pDebugMessenger);
+}
+
+void vkDestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT debugMessenger, const VkAllocationCallbacks* pAllocator) {
+    auto func = (PFN_vkDestroyDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkDestroyDebugUtilsMessengerEXT");
+    if (!func) {
+        return;
+    }
+
+    func(instance, debugMessenger, pAllocator);
+}
